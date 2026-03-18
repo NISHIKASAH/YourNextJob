f1cb09923f85a62393658001f787823d05e089a4
NISHIKA SAH <thefund9@gmail.com>
2026-03-18 20:55:26 +0530

    get current user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Auth from './pages/Auth.jsx'
+import axios from 'axios'
 
+
+export const ServerUrl  = "http://localhost:8000"
 const App = () => {
+
+
+  useEffect(()=>{
+    const  getUser = async()=>{
+      try{
+         const result =  await axios.get(ServerUrl + "/api/user/current-user" , {
+        withCredentials : true
+      });
+      console.log(result.data);
+      }catch(err){
+        console.log(err);
+      }
+    }
+    getUser();
+
+  } , [])
   return (
    <Routes>
     <Route  path='/' element= {<Home/>} />
--- server/index.js
+++ server/index.js
@@ -2,17 +2,26 @@ import express from "express"
 import dotenv from  "dotenv"
 import connectDb from "./config/connectDb.js";
 dotenv.config(); 
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
 
 
 const app =  express();
-app.get('/' , (req , res )=>{
-    return res.json({
-        message : "message send succesfully"
-    })
-})
+const PORT  =  process.env.PORT  || 6000 ; 
+app.use(cors({
+    origin : "http://localhost:5173",
+    credentials : true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth" , authRouter);
+app.use("/api/user" ,userRouter );
 
 
-app.listen(process.env.PORT ,()=>{
-    console.log(`server listen on port ${process.env.PORT}`);
+app.listen(PORT ,()=>{
+    console.log(`server listen on port ${PORT}`);
     connectDb();
 })
